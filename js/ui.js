dd83715431a27aa9609e00737f2369a27e8298b5
shhs0902 <70187019+shhs0902@users.noreply.github.com>
2022-06-04 02:07:42 +0900

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,82 @@
+'use strict';
 
-(function(window){
-  const hbgBtn = document.querySelector(".header__btn");
-  let boolean = false;
-  console.log(hbgBtn);
-  hbgBtn.addEventListener("click", function(){
-    if(!boolean) {
-      this.classList.add("on");
-      boolean = true;
-    }else {
-      this.classList.remove("on");
-      boolean = false;
-    }
+const hbgBtn = document.querySelector('.header__btn');
+let boolean = false;
+
+const NEXT_BTN = 'swiper-button-next';
+const PREV_BTN = 'swiper-button-prev';
+const ON_CLASS = 'on';
+
+function hbgBtnActive() {
+  if (!boolean) {
+    this.classList.add(ON_CLASS);
+    boolean = true;
+  } else {
+    this.classList.remove(ON_CLASS);
+    boolean = false;
+  }
+}
+const slideContainer = document.querySelector('.slide__container--myjs');
+const slideWrap = document.querySelector('.swiper-wrapper');
+const slide = document.querySelectorAll('.slide');
+const slideBtn = document.querySelectorAll('.slide__btn');
+const slideLenth = slide.length;
+let slideHeight;
+let current = 0;
+
+function slideHeightCalc() {
+  [].forEach.call(slide, function (s, i) {
+    slideHeight = s.offsetHeight;
   });
-}());
+}
 
+function slideEventHandler() {
+  [].forEach.call(slideBtn, function (b, i) {
+    b.addEventListener('click', function (e) {
+      slide[current].classList.remove(ON_CLASS);
+
+      if (e.target.classList.contains(NEXT_BTN)) {
+        nextBtnEventHandler();
+      }
+
+      if (e.target.classList.contains(PREV_BTN)) {
+        prevBtnEventHandler();
+      }
+    });
+  });
+}
+
+function nextBtnEventHandler() {
+  if (slideLenth - 1 > current) {
+    current = current + 1;
+    slide[current].classList.add(ON_CLASS);
+
+    goToSlide(current);
+  }
+}
+
+function prevBtnEventHandler() {
+  if (0 < current) {
+    current = current - 1;
+    slide[current].classList.add(ON_CLASS);
+
+    goToSlide(current);
+  }
+}
+
+function goToSlide(current) {
+  slideWrap.style.transform = `translate3d(0, ${-slideHeight * current}px, 0)`;
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  if (slideWrap) {
+    slideHeightCalc();
+  }
+  if (window.innerWidth > 1365) {
+    slideEventHandler();
+  } else {
+    return;
+  }
+
+  hbgBtn.addEventListener('click', hbgBtnActive);
+});
